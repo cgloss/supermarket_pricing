515ae1d2361cf790d62134587652f896c580dc9c
cgloss <cgloss1@gmail.com>
2018-09-22 13:40:17 -0400

    update comments to JSDoc

--- checkout.ts
+++ checkout.ts
@@ -3,40 +3,58 @@
 // making assumption to apply the most cost effective promotion for the customer
 // making assumption that overlaping promotions are intentional or acceptable
 
+/**
+ * Interface for pricing scheme.
+ *
+ * @interface
+ *
+ */
 interface IPricingScheme {
+  /** numeric auto inc id for each entry */
   _id: number;
+  /** string describes supermarket pricing scheme category */
   category: string;
+  /** string used to filter individual items and promotions from scheme */
   type: string;
+  /** array housing one or more sku for individual items */
   items: string[];
+  /** string describes the item or promotion */
   name: string;
+  /** numeric float which is applied to cart total can be either positive or negative */
   price: number;
+  /** optional numeric float can be applied to individual item as (1 * [tax]) - .0925 */
   tax?: number;
 }
 
+/**
+ * Class for an individual custom checkout.
+ * @constructor
+ * @param {Object[]} pricingScheme - the pricing scheme containing items and promotions.
+ */
 class Checkout {
 
-  public applicablePromotions: Array<IPricingScheme> = [];
-  public cacheTotal: number = 0;
   public cart: Array<IPricingScheme> = [];
+  private _applicablePromotions: Array<IPricingScheme> = [];
+  private _cacheTotal: number = 0;
   private _promotions: Array<IPricingScheme> = [];
 
   constructor(public pricingScheme: Array<IPricingScheme>) {
 
   }
   
-  // allow terminal to item/price check
-  itemCheck(sku: string): IPricingScheme {
+  /** allow terminal to item/price check */
+  itemCheck(sku: string | number): IPricingScheme | null{
     try {
-      return this.pricingScheme.find(entry => entry.type === 'item' && entry.items.length === 1 && entry.items[0] === sku);
+      return this.pricingScheme.find(entry => entry.type === 'item' && entry.items.length === 1 && entry.items[0] === String(sku));
     } catch (err) {
       console.log('item not found');
       return null;
     }
   }
 
-  // returns price for terminal use to display as items are scanned
-  scan(sku: string): number {
-    this.cacheTotal = 0;
+  /** returns price for terminal use to display as items are scanned */
+  scan(sku: string | number): number {
+    this._cacheTotal = 0;
     let item = this.itemCheck(sku);    
     if(!item){
       return null;
@@ -47,21 +65,26 @@ class Checkout {
     return (item.price * (1 + item.tax));
   }
 
-  // allow terminal to look up item related promotions
-  findPromotions(sku: string): Array<IPricingScheme> {
-    let promotions = this.pricingScheme.filter(entry => entry.type === 'promo' && entry.items.length >= 1 && entry.items.indexOf(sku) !== -1);
+  /** allow terminal to look up item related promotions */
+  findPromotions(sku: string | number): Array<IPricingScheme> {
+    let promotions = this.pricingScheme.filter(entry => entry.type === 'promo' && entry.items.length >= 1 && entry.items.indexOf(String(sku)) !== -1);
     // concat filtered promotions relevant to this sku
     this._promotions = this._promotions.concat(promotions);
     return promotions;
   }
 
-  // allow terminal to get current scanned items
+  /** allow terminal to get current scanned items */
   reviewCart(): Array<IPricingScheme> {
     return this.cart;
   }
 
+  /**
+   * filters and sorts promotions related to current cart items 
+   * then validates that all conditions of the promotion are met 
+   * adds to applicable promotions for use in final total 
+   */
   private validatePromotions(): void {
-    this.applicablePromotions = <IPricingScheme[]>[] 
+    this._applicablePromotions = <IPricingScheme[]>[] 
     // define array of cart sku's to ensure no erroneous duplication of shared partials in promotions
     let available = this.cart.map(entry => entry.items[0]);
     // sort promotions by cost savings in the event of duplicate/overlapping promos
@@ -83,40 +106,41 @@ class Checkout {
       }
       // verify all nessesary conditions are met
       if (valid.size === found.items.length) {
-        this.applicablePromotions.push(found);
+        this._applicablePromotions.push(found);
       }
     } 
   }
 
-  // allow terminal to void item from cart
-  void(sku: string): void {
-    this.cacheTotal = 0;
-    let index = this.cart.findIndex(entry => entry.items[0] === sku);
-    console.log(index);
+  /** allow terminal to void item from cart */
+  void(sku: string | number): void {
+    this._cacheTotal = 0;
+    let index = this.cart.findIndex(entry => entry.items[0] === String(sku));
     if(index !== -1){
       this.cart.splice(index, 1);
     }
   }
 
-  // allow terminal to get the promotions applied to the current cart
+  /** allow terminal to get the promotions applied to the current cart */
   getAppliedPromotions(): Array<IPricingScheme>{
-    return this.applicablePromotions;
+    return this._applicablePromotions;
   }
 
+  /** return item price with applicable tax included */
   getSum(item:IPricingScheme): number {
     return (item.price * (1 + item.tax));
   }
 
+  /** Returns the cart total with promotional reductions applied */
   getTotal(): number {
-    if (!this.cacheTotal) {
+    if (!this._cacheTotal) {
       let total = this.cart.reduce((a, b) => a + this.getSum(b), 0);
       // apply promotions
       this.validatePromotions();
-      let reduction = this.applicablePromotions.reduce((a, b) => a + b.price, 0);
+      let reduction = this._applicablePromotions.reduce((a, b) => a + b.price, 0);
       // assumption return in cents per document
-      this.cacheTotal = Math.round((total+reduction)*100);
+      this._cacheTotal = Math.round((total+reduction)*100);
     }
-    return this.cacheTotal;
+    return this._cacheTotal;
 
   }
 }
